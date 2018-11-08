5b97689af18ab58c440618598931db8a7642f12a
Xiaoyun <zxiaoyun98@gmail.com>
2018-11-09 01:04:41 +0800

    created EventNew form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import Slideshow from './components/slideshow'
+import EventNew from './components/event_new';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Slideshow />
+        <Switch>
+          <Route path='/new' component={EventNew} />
+        </Switch>
+        {/* <Slideshow /> */}
+        {/* <EventNew /> */}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import promise from 'redux-promise';
+import reducers from './reducers'
+
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+const app = (
+  <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
