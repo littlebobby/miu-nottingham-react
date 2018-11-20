2552034ddc7aada96b7f524c7680aaaf236af0e1
Xiaoyun <zxiaoyun98@gmail.com>
2018-11-21 02:24:05 +0800

    CONFIGURE FIREBASE

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCd5YR1D2yOm1W9A1ZLbS266QKVYv1kukk",
+  authDomain: "miu-nottingham.firebaseapp.com",
+  databaseURL: "https://miu-nottingham.firebaseio.com",
+  projectId: "miu-nottingham",
+  storageBucket: "miu-nottingham.appspot.com",
+  messagingSenderId: "980884905409"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
