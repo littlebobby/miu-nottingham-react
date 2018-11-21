f78b85f9a3b5a5d47f47ebc77153f5acadeeda6e
Xiaoyun <zxiaoyun98@gmail.com>
2018-11-21 16:42:48 +0800

    firebase logout done

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './NavLinks.module.css';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions'
 
 
-export default function SignedInLinks() {
+
+function SignedInLinks(props) {
   return (
     <ul className={styles.ul}>
       <li><NavLink className={styles.a} to='/create'>New Project</NavLink></li>
-      <li><NavLink className={styles.a} to='/'>Log Out</NavLink></li>
+      <li><a onClick={props.signOut} className={styles.a}>Log Out</a></li>
       <li><NavLink className={styles.a} to='/'>NNN</NavLink></li>
     </ul>
   )
 }
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks)
