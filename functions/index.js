dc5d66ddf592da23e3eeb643e590c392e101ece2
Xiaoyun <zxiaoyun98@gmail.com>
2018-11-21 21:29:43 +0800

    cloud functions triggers

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,43 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const createNotification = ((notification) => {
+  return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notification added', doc));
+});
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello bobby");
+
+exports.eventCreated = functions.firestore
+  .document('events/{eventId}')
+  .onCreate(doc => {
+
+    const event = doc.data();
+    const notification = {
+      content: 'Added a new event',
+      user: `${event.authorFirstName} ${event.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
+
+    return createNotification(notification);
+
+});
+
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+    
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return createNotification(notification);
+
+      });
 });
