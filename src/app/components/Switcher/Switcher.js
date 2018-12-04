4bbd4673d30260004e1577770fe49b006b565554
Xiaoyun <zxiaoyun98@gmail.com>
2018-12-04 21:31:03 +0800

    styling for event type switcher

--- src/app/components/Switcher/Switcher.js
+++ src/app/components/Switcher/Switcher.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './Switcher.module.scss'
 import { connect } from 'react-redux'
 import { updateEventType } from '../../store/actions/eventTypeActions'
 
-function Switcher(props) {
-  console.log(props.eventType)
-  return (
-    <div className={styles.container} >
-      <span onClick={() => props.updateType('all')} className={styles.item}>All</span>
-      <span onClick={() => props.updateType('party')} className={styles.item}>Party</span>
-      <span onClick={() => props.updateType('lecture')} className={styles.item}>Lecture</span>
-      <span onClick={() => props.updateType('career')} className={styles.item}>Career</span>
-    </div>
-  )
+const Switcher = (props) => { 
+    console.log(props.eventType)
+    return (
+
+      <div className={styles.container} >
+        <div onClick={() => props.updateType('all')} className={props.eventType === 'all' ? styles.Highlighted : ''}><span>All</span></div>
+        <div onClick={() => props.updateType('party')} className={props.eventType === 'party' ? styles.Highlighted : ''}><span>Party</span></div>
+        <div onClick={() => props.updateType('lecture')} className={props.eventType === 'lecture' ? styles.Highlighted : ''}><span>Lecture</span></div>
+        <div onClick={() => props.updateType('career')} className={props.eventType === 'career' ? styles.Highlighted : ''}><span>Career</span></div>
+      </div>
+    )
+  
 }
 
 const mapStateToProps = state => {
-  console.log(state)
   return {
-    eventType: state.eventype
+    eventType: state.eventType
   }
 }
 
