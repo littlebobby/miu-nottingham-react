7d3369c3ce8721e50ee274fbe5424d7638f457f5
Xiaoyun <zxiaoyun98@gmail.com>
2018-12-04 22:01:05 +0800

    slideshow for mobile

--- src/app/components/Slideshow/Slide/Slide.js
+++ src/app/components/Slideshow/Slide/Slide.js
@@ -3,9 +3,9 @@ import cssModules from './Slide.module.scss'
  export default function Slide(props) {
   const styles = {
 		backgroundImage: `url(${props.image})`,
-		backgroundSize: 'cover',
-		backgroundRepeat: 'no-repeat',
-		height: '80vh',
+		// backgroundSize: 'cover',
+		// backgroundRepeat: 'no-repeat',
+		// height: '80vh',
 	} 
   return (
     <div className={cssModules.slide} style={styles}>
